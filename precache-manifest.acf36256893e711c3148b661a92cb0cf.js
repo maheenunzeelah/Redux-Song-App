f4feddcac3835337e39102d0a0a90e164d780e1c
Maheen Unzeelah <46377344+maheenunzeelah@users.noreply.github.com>
2020-02-14 20:16:31 +0500

    Updates

---
+++ precache-manifest.acf36256893e711c3148b661a92cb0cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11547b489b684dba20831d4cef9136f9",
+    "url": "/Redux-Song-App/index.html"
+  },
+  {
+    "revision": "f1e25b7fd1f7d785537b",
+    "url": "/Redux-Song-App/static/js/2.30522806.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Redux-Song-App/static/js/2.30522806.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fd2b8fed3af753dc796",
+    "url": "/Redux-Song-App/static/js/main.bb0075de.chunk.js"
+  },
+  {
+    "revision": "e7df714529aba051e28a",
+    "url": "/Redux-Song-App/static/js/runtime-main.01e45cf0.js"
+  }
+]);
